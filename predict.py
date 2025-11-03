5b673754bc14c5f273451b37726fd582686d7d12
Denis <denshe@gmail.com>
2025-11-03 20:50:17 +0000

    hw5

---
+++ predict.py
@@ -0,0 +1,29 @@
+import pickle
+from typing import Dict, Any
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(title="customer-churn-prediction")
+
+with open('pipeline_v2.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+
+def predict_single(customer):
+    result = pipeline.predict_proba(customer)[0, 1]
+    return float(result)
+
+
+
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    prob = predict_single(customer)
+
+    return {
+        "churn_probability": prob,
+        "churn": bool(prob >= 0.5)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
